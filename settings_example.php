c558c2245bbab6812258c2ce6506adce7267f29b
edrabbit <edrabbit@edrabbit.com>
2012-08-12 21:07:45 -0400

    Use .bio as extension for valid activities so we can add to .gitignore

--- settings_example.php
+++ settings_example.php
@@ -1,6 +1,6 @@
 <?
 // valid_activities.txt is a file with the string for each valid activity on it's own line
-define(ACTIVITIES_FILE, "/path/to/valid_activities.txt");
+define(ACTIVITIES_FILE, "/path/to/valid_activities.bio");
 
 // The location of the file to log all activities to
 define(ACTIVITIES_LOG_FILE, '/path/to/activities.log');
